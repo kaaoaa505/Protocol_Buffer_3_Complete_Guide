692159669da3a12bfc9f5aaa99abb4b7ca3e9a06
Khaled Allam <kaaoaa505@gmail.com>
2023-06-20 13:47:14 +0300

    Build stream from proto object Save output stream to sample_output.bin

--- project-java/protobuf-java-project/src/main/java/org/example/Main.java
+++ project-java/protobuf-java-project/src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import dev.sample.local.Sample.SampleObj;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        SampleObj.Builder builder = SampleObj.newBuilder();
+        builder.setId(123)
+                .setName("Khaled Allam")
+                .setIsValid(true)
+                .addAllStrList(Arrays.asList("Khaled", "Allam", "Ahmed", "Omar", "Abdalla", "Allam"));
+
+        System.out.println(builder.getName());
+        System.out.println(builder.getStrList(2));
+        System.out.println(builder.getStrList(3));
+        System.out.println(builder.getStrList(4));
+
+        SampleObj message = builder.build();
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream("sample_output.bin");
+            message.writeTo(outputStream);
+            outputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        byte[] targetBytes = message.toByteArray();
+        System.out.println(targetBytes);
     }
 }
