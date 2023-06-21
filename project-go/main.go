cab9837875b45c21a932553c56971335408f604d
Khaled Allam <kaaoaa505@gmail.com>
2023-06-21 14:36:26 +0300

    Generate and use local packages for proto Import local by:      local.dev/m/v2/build/local Access object by:     local.SampleObj

--- project-go/main.go
+++ project-go/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"local.dev/m/v2/build/local"
+)
 
 func main() {
 	fmt.Println("Hello World!.")
+
+	call_sample_message()
+}
+
+func call_sample_message() {
+	sm := local.SampleObj{
+		Id:      0,
+		Name:    "",
+		IsValid: false,
+		StrList: []string{"str1", "str2", "str3"},
+	}
+
+	sm.IsValid = true
+	sm.Id = 123
+	sm.Name = "Khaled Allam"
+
+	fmt.Println(sm.IsValid)
+	fmt.Println(sm.Id)
+	fmt.Println(sm.Name)
+	fmt.Println(sm.StrList)
 }
