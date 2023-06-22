ea7a83242a73725189bcfd62398df22863d5b59a
Khaled Allam <kaaoaa505@gmail.com>
2023-06-22 13:32:53 +0300

    Read binary data from file Convert binary data to SampleObject Print the content of the new SampleObject

--- project-python/main.py
+++ project-python/main.py
@@ -12,9 +12,19 @@ message.str_list.append('omar')
 message.str_list.append('abdalla')
 message.str_list.append('allam')
 
+print('---- Write to binary file')
 print(message)
-
 # wb: mean Write-Binary
 with open('message.bin', 'wb') as f:
     bytesAsString = message.SerializeToString()
+    print('---- Write to binary file bytesAsString is: \n\t', bytesAsString)
     f.write(bytesAsString)
+
+print('\n\n---- Read from binary file')
+# rb: mean Read-Binary
+with open('message.bin', 'rb') as f:
+    messageRead = f.read()
+    print('---- Read from binary file messageRead is: \n\t', messageRead)
+    messageReadString = sample_pb2.SampleObj().FromString(messageRead)
+    print(messageReadString)
+
