f21e2c1a599feb46a107efdb14ec435a5897d76b
Khaled Allam <kaaoaa505@gmail.com>
2023-06-22 09:49:20 +0300

    Write to binary file and read from it Read proto and convert it to json string Convert json string to proto object Display fields from proto object

--- project-go/main.go
+++ project-go/main.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
 	"strings"
 
+	"github.com/golang/protobuf/jsonpb"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/runtime/protoiface"
 	"local.dev/m/v2/build/local"
 )
 
 func main() {
 	println("Hello World!.")
 
-	message := call_sample_message()
+	messageToWrite := call_sample_message()
+	writeToFile("message.bin", messageToWrite)
 
-	println(message)
+	messageToRead := &local.SampleObj{}
+	data := readFromFile("message.bin", messageToRead)
 
-	writeToFile("message.bin", message);
+	println("Data bytes array from read file is: ", data)
+
+	println("\n---- Note: you need to use marshal to get the complete correct data")
+	println("\tData from byte to string: ", string(data[:]))
+
+	toJsonResult, toJsonError := protoToJson(messageToRead)
+
+	if toJsonError != nil {
+		println(toJsonError)
+		log.Fatalln(toJsonError)
+	}
+
+	println("messageToRead protoToJson: ", toJsonResult)
+
+	messageToProto := &local.SampleObj{}
+	toProtoResult := jsonToProto(strings.NewReader(toJsonResult), messageToProto)
+	println("messageToRead toProtoResult: ", toProtoResult)
+	println("messageToRead messageToProto: ", messageToProto)
+	println("messageToRead messageToProto.Id: ", messageToProto.Id)
+	println("messageToRead messageToProto.Name: ", messageToProto.Name)
+	println("messageToRead messageToProto.IsValid: ", messageToProto.IsValid)
 }
 
-func call_sample_message() *local.SampleObj{
+func call_sample_message() *local.SampleObj {
 	sm := local.SampleObj{
 		Id:      0,
 		Name:    "",
@@ -39,26 +64,60 @@ func call_sample_message() *local.SampleObj{
 	return &sm
 }
 
-func writeToFile(fileName string, message proto.Message) error{
+func writeToFile(fileName string, message proto.Message) error {
 	println(fileName)
-	output, messageError := proto.Marshal(message);
+	output, messageError := proto.Marshal(message)
 
-	if(messageError != nil){
+	if messageError != nil {
 		println(messageError)
-		log.Fatalln(messageError);
-		return messageError;
+		log.Fatalln(messageError)
+		return messageError
 	}
 
 	println(output)
 
-	ioWriteError := ioutil.WriteFile(fileName, output, 0644);
-	if(ioWriteError != nil){
+	ioWriteError := ioutil.WriteFile(fileName, output, 0644)
+	if ioWriteError != nil {
 		println(ioWriteError)
-		log.Fatalln(ioWriteError);
-		return ioWriteError;
+		log.Fatalln(ioWriteError)
+		return ioWriteError
 	}
 
 	println("Write to file was successful, File Name: ", fileName)
 
-	return nil;
+	return nil
+}
+
+func readFromFile(fileName string, message proto.Message) []byte {
+
+	data, ioReadError := ioutil.ReadFile(fileName)
+
+	if ioReadError != nil {
+		println(ioReadError)
+		log.Fatalln(ioReadError)
+		return nil
+	}
+
+	println("Read from file was successful, File Name: ", fileName)
+
+	proto.Unmarshal(data, message)
+
+	return data
+}
+
+func protoToJson(message protoiface.MessageV1) (string, error) {
+	marshaler := jsonpb.Marshaler{
+		EnumsAsInts:  false,
+		EmitDefaults: true,
+		Indent:       "  ",
+		OrigName:     true,
+	}
+
+	return marshaler.MarshalToString(message)
+}
+
+func jsonToProto(data io.Reader, message protoiface.MessageV1) error{
+	unmarshalResult := jsonpb.Unmarshal(data, message)
+
+	return unmarshalResult
 }
